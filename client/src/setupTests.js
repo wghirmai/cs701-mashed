c1ae0be7a3f207abe6a458a70dde1a7002cd2254
Elva Osorio <osorioelva@gmail.com>
2020-10-18 18:41:24 -0400

    server/client

--- client/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import "jest-enzyme";
-
-configure({ adapter: new Adapter() });
--- server/routes.test.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('./routes');
-
-test('Server "smoke" test', () => {
-  expect(app).toBeDefined();
-});
-
-test('dotenv configured', () => {
-  expect(process.env.MY_SECRET).toBeDefined();
-});
