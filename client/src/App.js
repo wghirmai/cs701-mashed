dd8ea3ed9e0a48a406b5b45db46a21e56ae54872
Winta Ghirmai <wghirmai@middlebury.edu>
2020-11-04 15:26:52 -0500

    Access Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
 import Login from "./Login.js";
-
+import { getTokenFromResponse } from "./spotify";
+//<img src={logo} className="App-logo" alt="logo" />
 const App = () => {
+  //Run code based on a condition
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    setToken(hash.access_token);
+    //window.location.hash="";
+    console.log("My Access Token:", token);
+  }, [token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to MASHED</h1>
-      </header>
-      <p className="App-intro">API here</p>
-      <Login> </Login>
+      <h1 className="App-title">Welcome to MASHED</h1>
+
+      {token ? <h1 className="App-title">Logged in</h1> : <Login />}
     </div>
   );
 };
