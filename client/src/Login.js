ea3a185ccc399257f7080f3418142975c3d04713
Winta Ghirmai <wghirmai@middlebury.edu>
2020-11-19 15:06:42 -0500

    Updated spotify api routes, brought dataset to frontend

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Login.css";
-import { accessUrl } from "./spotify";
-function Login() {
+//import { accessUrl } from "./spotify";
+//import { accessUrl } from "./spotify";
+// <a href={accessUrl}> LOGIN WITH SPOTIFY</a>
+function Login({ user, artists }) {
   return (
     <div className="login">
-      <a href={accessUrl}> LOGIN WITH SPOTIFY</a>
+      <a href={"http://localhost:3001/login"}> Login to Spotify </a>
     </div>
   );
 }
